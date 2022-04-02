6c976b1f8f4ab116027933e8057f76cee5fe9e9a
St3ph4ne <spodar.s@protonmail.com>
2022-04-02 09:21:37 +0000

    routage

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ app.get('/' , (req,res) => {
   res.send('Hello world !');
 });
 
+app.get('/movies' , (req,res) => {
+  res.send('Liste des films');
+});
+
+app.get('/movies/add' , (req,res) => {
+  res.send('prochainement un formulaire d\'ajout ici');
+});
+
+app.get('/movies/:id' , (req,res) => {
+  const id = req.params.id;
+  res.send(`Détail du film ${id}`);
+});
+
 app.listen(PORT , () => {
   console.log(`listening on port PORT`);
 });
