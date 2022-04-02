e42d9e35dfef95b27ee0773fcdb379c9bf3d8013
St3ph4ne <spodar.s@protonmail.com>
2022-04-02 09:46:59 +0000

    créer un template avec ejs

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// ajout du middleware express.static pour indiquer l'emplacement des fichiers à servir statiquement (ici style.css)
+app.use('/public' , express.static('public'))
+
+// indique le view engine et son chemin
+app.set('views' , './views');
+app.set('view engine' , 'ejs');
+
 app.get('/' , (req,res) => {
-  res.send('Hello world !');
+  // res.send('Hello world !');
+  res.render('index.ejs');
 });
 
 app.get('/movies' , (req,res) => {
